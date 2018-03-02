2d8b2ae75e12ed9f9c1f2576ff3257b43ce7c620
jkrowden <jkrowden@gmail.com>
2018-03-01 17:22:25 -0700

    finished app functionality, still some bugs to work out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,27 @@
 var search = ['google', 'deadmau5', 'blink-182'];
+var images = [];
 var gifClicked = false;
+var lastImageClicked;
+var searchTerm = '';
 
 $(document).ready(function () {
 	console.log("ready");
 	createButtons();
 });
 
+$(document).on("click", ".search-button", function () {
+	searchTerm = $(this).text();
+	console.log($(this).text());
+	generateImages();
+	createButtons(searchTerm);
+	images = [];
+});
+
 $(document).on("click", "#btn-search", function () {
-	getImage($.trim($('#search-input').val()));
-	createButtons($.trim($('#search-input').val()));
+	searchTerm = $.trim($('#search-input').val());
+	generateImages();
+	createButtons(searchTerm);
+	images = [];
 });
 
 $(document).on("click", ".search-del-button", function () {
@@ -17,19 +30,27 @@ $(document).on("click", ".search-del-button", function () {
 	var index = search.indexOf($(button).attr('search-term'));
 	search.splice(index, 1);
 
-	
-
 	$(this).parent().remove();
+	generateImages();
 });
 
 $(document).on("click", ".gif-image", function () {
 	if (!gifClicked) {
+		$(this).attr('src', $(this).attr('animated'));
 		$(this).animate({ width: '33vh' });
+		lastImageClicked = this;
 		gifClicked = true;
 	}
-	else {
-		$(this).animate({ width: '21vh'});
+	else if (lastImageClicked === this) {
+		$(this).animate({ width: '21vh' });
+		$(this).attr('src', $(this).attr('still'));
 		gifClicked = false;
+	} else {
+		$(lastImageClicked).animate({ width: '21vh' });
+		$(lastImageClicked).attr('src', $(lastImageClicked).attr('still'));
+		gifClicked = false;
+		$(this).animate({ width: '33vh' });
+		$(this).attr('src', $(this).attr('animated'));
 	}
 
 });
@@ -75,25 +96,38 @@ function getImage(term) {
 
 	$.ajax({
 		type: "GET",
-		url: "https://api.giphy.com/v1/gifs/random?api_key=ItHMCAF2beAU5uC3GtFXENruGY8CvR96&qtag=" + term + "&limit=3"
+		url: "https://api.giphy.com/v1/gifs/random?api_key=ItHMCAF2beAU5uC3GtFXENruGY8CvR96&tag=" + searchTerm + "&rating=G"
 	}).then(function (response) {
-		console.log(response.data.images.fixed_width.url);
-		addImage(response.data.images.fixed_width.url);
+		addImage(response.data.images.fixed_width_still.url, response.data.images.fixed_width.url);
 	});
-
 
 }
 
-function addImage(image) {
+function addImage(stillImage, animated) {
 	var tempImage = $("<img>");
-	$(tempImage).attr('src', image);
+	$(tempImage).attr('src', stillImage);
+	$(tempImage).attr('still', stillImage);
+	$(tempImage.attr('animated', animated));
 	$(tempImage).css('width', '21vh');
 	$(tempImage).addClass('gif-image');
 
-	$('.images').append(tempImage);
+	images.push(tempImage);
+	console.log(images);
+	appendImages();
 }
 
-function expandImage(image) {
-
+function generateImages() {
+	for (var i = 0; i < 10; ++i) {
+		getImage(searchTerm);
+	}
+	console.log(images);
 }
 
+function appendImages() {
+
+	$('.images').empty();
+	$.each(images, function (index, current) {
+		$('.images').append(current);
+		console.log('gotimage');
+	});
+}
